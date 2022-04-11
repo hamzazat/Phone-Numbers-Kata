43893ca78647856bc88a83bffa3a7a2931ce18a1
Hamza <32413751+hamzazat@users.noreply.github.com>
2022-04-11 13:20:28 +0300

    add PhoneBook and unittest

---
+++ phonebook/phonebook.py
@@ -0,0 +1,20 @@
+
+
+class PhoneBook():
+    def __init__(self) -> None:
+        self.list = {}
+
+    def add(self, name: str, phone: str):
+        self.list[name] = phone
+
+    def lookup(self, name: str):
+        return self.list[name]
+
+    def is_consistant(self):
+        for name1, number1 in self.list.items():
+            for name2, number2 in self.list.items():
+                if name1 == name2:
+                    continue
+                if  number1.startswith(number2):
+                    return False
+        return True
---
+++ unittest/test_phonebook.py
@@ -0,0 +1,43 @@
+import unittest
+
+
+import unittest
+from phonebook import PhoneBook
+
+
+class PhoneBookTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.phonebook = PhoneBook()
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_lookup_by_name(self):
+        self.phonebook.add("Hamza", "12345")
+        number = self.phonebook.lookup("Hamza")
+        self.assertEqual("12345", number)
+
+    def test_missing_name(self):
+        with self.assertRaises(KeyError):
+            self.phonebook.lookup("missing")
+
+    def test_empty_phonebook_is_consistent(self):
+        self.assertTrue(self.phonebook.is_consistant())
+
+    def test_is_consistent_with_different_entries(self):
+        self.phonebook.add("Test 0", "12345")
+        self.phonebook.add("Test 1", "012345")
+        self.assertTrue(self.phonebook.is_consistant())
+
+    def test_inconsistant_with_duplicate_enteries(self):
+        self.phonebook.add("Test 0", "12345")
+        self.phonebook.add("Test 1", "12345")
+        self.assertFalse(self.phonebook.is_consistant())
+
+    def test_incosistant_with_duplicat_prefix(self):
+        self.phonebook.add("Test 0", "12345")
+        self.phonebook.add("Test 1", "123")
+        self.assertFalse(self.phonebook.is_consistant())
+
+    def test_phonebook_adds_names_and_numbers(self):
+        pass
